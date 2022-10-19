b74c6ebf4b8eaa4cc7e70fbfba4bd48cd8bc93f3
Dmitriy Angve <kirirch129@gmail.com>
2022-10-19 14:09:49 +0300

    #

--- script.js
+++ script.js
@@ -51,16 +51,49 @@
 // console.log(age1, age2);
 
 // Arrow function
-const calcAge3 = (birthYear) => 2037 - birthYear;
-const age3 = calcAge3(1991);
-console.log(age3);
+// const calcAge3 = (birthYear) => 2037 - birthYear;
+// const age3 = calcAge3(1991);
+// console.log(age3);
 
-const yearUntilRetirement = (birthYear, firstName) => {
-  const age = 2037 - birthYear;
-  const retirement = 65 - age;
-  //   return retirement;
-  return `${firstName} retires in ${retirement} years`;
-};
+// const yearUntilRetirement = (birthYear, firstName) => {
+//   const age = 2037 - birthYear;
+//   const retirement = 65 - age;
+//   //   return retirement;
+//   return `${firstName} retires in ${retirement} years`;
+// };
 
-console.log(yearUntilRetirement(1991, "Dmitriy"));
-console.log(yearUntilRetirement(1980, "Bob"));
+// console.log(yearUntilRetirement(1991, "Dmitriy"));
+// console.log(yearUntilRetirement(1980, "Bob"));
+
+// function cutFruitPieces(fruit) {
+//   return fruit * 4;
+// }
+
+// function fruitProcessor(apples, oranges) {
+//   const applePieces = cutFruitPieces(apples);
+//   const orangePieces = cutFruitPieces(oranges);
+
+//   console.log(apples, oranges);
+//   const juice = `Juice with ${applePieces} piece of apples and ${orangePieces} piece of oranges.`;
+//   return juice;
+// }
+// console.log(fruitProcessor(2, 3));
+
+// const calcAge = function (birthYear) {
+//   return 2037 - birthYear;
+// };
+
+// const yearUntilRetirement = function (birthYear, firstName) {
+//   const age = calcAge(birthYear);
+//   const retirement = 65 - age;
+
+//   if (retirement > 0) {
+//     console.log(`${firstName} retires in ${retirement} years`);
+//     return retirement;
+//   } else {
+//     console.log(`${firstName} has already retired!`);
+//     return -1;
+//   }
+// };
+// console.log(yearUntilRetirement(1991, "Dmitriy"));
+// console.log(yearUntilRetirement(1950, "Mike"));
