8318b390ccdba2873b57d1cc9f90a9a29ddbf278
Dmitriy Angve <kirirch129@gmail.com>
2022-10-19 13:09:29 +0300

    #

--- script.js
+++ script.js
@@ -34,6 +34,18 @@
 
 // const num = Number("23"); // function too
 
-function calcAge1(birthYear) {
-  return 2037 - birthYear;
-}
+// // Function declaration
+// function calcAge1(birthYear) {
+//   return 2037 - birthYear;
+// }
+
+// const age1 = calcAge1(1991);
+// console.log(age1);
+
+// // Function expression
+// const calcAge2 = function (birthYear) {
+//   return 2037 - birthYear;
+// };
+
+// const age2 = calcAge2(1991);
+// console.log(age1, age2);
