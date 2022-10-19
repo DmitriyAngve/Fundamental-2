610be1ae73a9392761f9a943a134ea48e896c9cc
Dmitriy Angve <kirirch129@gmail.com>
2022-10-19 12:45:36 +0300

    #

---
+++ script.js
@@ -0,0 +1,11 @@
+// "use strict";
+
+// let hasDriversLicence = false;
+// const passTest = true;
+
+// if (passTest) hasDriversLicence = true;
+// if (hasDriversLicence) console.log("I can drive");
+
+// // const interface = "Audio"; // strict mode does not allow to use this words
+// // const private = 534; // strict mode does not allow to use this words
+// // const if = 23; // strict mode does not allow to use this words
