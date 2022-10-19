915b3f9f3f5abd8e94f98a9d0a428925e5396e3b
Dmitriy Angve <kirirch129@gmail.com>
2022-10-19 13:00:37 +0300

    #

--- script.js
+++ script.js
@@ -9,3 +9,31 @@
 // // const interface = "Audio"; // strict mode does not allow to use this words
 // // const private = 534; // strict mode does not allow to use this words
 // // const if = 23; // strict mode does not allow to use this words
+
+// function logger() {
+//   console.log("My name is Dmitriy");
+// }
+
+// // calling / running / invoking function
+// logger();
+// logger();
+// logger();
+
+// function fruitProcessor(apples, oranges) {
+//   console.log(apples, oranges);
+//   const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
+//   return juice;
+// }
+
+// const appleJuice = fruitProcessor(5, 0);
+// console.log(appleJuice);
+// // console.log(fruitProcessor(5, 0));
+
+// const appleOrangeJuice = fruitProcessor(2, 4);
+// console.log(appleOrangeJuice);
+
+// const num = Number("23"); // function too
+
+function calcAge1(birthYear) {
+  return 2037 - birthYear;
+}
